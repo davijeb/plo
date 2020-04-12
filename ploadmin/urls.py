3d63c5532fcec9ed6454c8f76b0c9d5405d2932a
Jez <jerdavies@gmail.com>
2020-04-12 15:10:18 +0100

    Added details, index and templates

--- ploadmin/urls.py
+++ ploadmin/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ploadmin'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.test, name='test'),
+    path('detail/<int:user_id>', views.detail, name='detail'),
+    path('<int:user_id>/results/', views.results, name='results'),
+    path('<int:user_id>/vote/', views.vote, name='vote'),
+    path('index', views.index, name='index'),
 ]
--- ploadmin/views.py
+++ ploadmin/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
+from .models import User
+
+def test(request):
+	print('hello')
+	return HttpResponse("Hello...")
+
+def detail(request, user_id): 
+	user = get_object_or_404(User, pk=user_id)
+	return render(request, 'ploadmin/detail.html', {'user': user})
+
+def results(request, user_id):
+	return HttpResponse("You are looking at the results for question {}".format(user_id))
+
+def vote(request, user_id):
+	return HttpResponse("You are voting on question {}".format(user_id))
 
 def index(request):
-    return HttpResponse("Hello, Dr Mak.")
-
-def createSuperUser(request):
-    admin = os.system("python manage.py createsuperuser")
-    return 
+	print('***********')
+	latest_user = User.objects.order_by('id')[:5]
+	return render(request, 'ploadmin/index.html', {'latest_user': latest_user})
