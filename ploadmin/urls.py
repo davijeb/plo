5b3536a58803a1c089194d46e2f7e27379dbf733
Jez <jerdavies@gmail.com>
2020-04-12 18:58:05 +0100

    Basic form entry to change email and simple non-styled table

--- ploadmin/urls.py
+++ ploadmin/urls.py
@@ -7,7 +7,7 @@ app_name = 'ploadmin'
 urlpatterns = [
     path('', views.test, name='test'),
     path('detail/<int:user_id>', views.detail, name='detail'),
-    path('<int:user_id>/results/', views.results, name='results'),
-    path('<int:user_id>/vote/', views.vote, name='vote'),
+    path('results/', views.results, name='results'),
+    path('modify/<int:user_id>', views.modify, name='modify'),
     path('index', views.index, name='index'),
 ]
--- ploadmin/views.py
+++ ploadmin/views.py
@@ -1,23 +1,39 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+import django_tables2 as tables
 
 from .models import User
 
+class SimpleTable(tables.Table):
+    class Meta:
+        model = User
+
+
 def test(request):
-	print('hello')
 	return HttpResponse("Hello...")
 
 def detail(request, user_id): 
 	user = get_object_or_404(User, pk=user_id)
 	return render(request, 'ploadmin/detail.html', {'user': user})
 
-def results(request, user_id):
-	return HttpResponse("You are looking at the results for question {}".format(user_id))
+def results(request):
+    table = SimpleTable(
+        User.objects.all(), attrs={"class": "paleblue"}, template_name="django_tables2/table.html"
+    )
+    return render(request, "ploadmin/results.html", {"table": table})
 
-def vote(request, user_id):
-	return HttpResponse("You are voting on question {}".format(user_id))
+def modify(request, user_id):
+	user = get_object_or_404(User, pk=user_id)
+	email = request.POST['email']
+	print('Email is', email)
+	user.email = email
+	user.save()
+
+	return HttpResponseRedirect(reverse('ploadmin:index'))
+
 
 def index(request):
-	print('***********')
 	latest_user = User.objects.order_by('id')[:5]
 	return render(request, 'ploadmin/index.html', {'latest_user': latest_user})
